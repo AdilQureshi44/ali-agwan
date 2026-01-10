b58d4c9c4c0a883ccaaa7e4fe44cca3f17631cfd
Adil Qureshi <adil.aq48@gmail.com>
2026-01-11 01:53:56 +0300

    full code in wesite

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { Hero } from './section/hero/hero';
+import { Service } from './section/service/service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer,Hero],
+  imports: [RouterOutlet,Header,Footer,Hero,Service],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/section/hero/hero.ts
+++ src/app/section/hero/hero.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Service } from '../service/service';
+import { About } from '../about/about';
+import { Contact } from '../contact/contact';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [Service,About,Contact],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
